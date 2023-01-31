bf49792e0c0bad97d68bd544f5dc8bdf9754c61e
TAKAMURAFUMIHITO <108682238+TAKAMURAFUMIHITO@users.noreply.github.com>
2023-01-31 17:43:47 +0900

    エンドポイント, ルート, ミドルウェア

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 const express = require('express');
 const app = express();
+const userRouter = require("./routes/user");
+
 const port = 8000;
+
+// app.use(mylogger);
+// app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   // console.log("hello express");
   // res.send("hello express");
   // res.sendStatus(404);
   // res.status(200).send("hello express");
-  res.status(500).json({ msg: "エラーです。" });
+  // res.status(500).json({ msg: "エラーです。" });
+  res.render('index', { text: "こんにちは" });
+  // renderでストップ
 });
 
-app.get('/user', (req, res) => {
-  res.send("ユーザーです。");
-});
-
-app.get('/user/info', (req, res) => {
-  res.send("ユーザー情報です。");
-});
+app.use("/user", userRouter);
 
 app.listen(port, () => console.log("サーバーが起動しました。"));
